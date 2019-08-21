9df1a74e1c8fd822d004134d0d2d56397b641e45
shengjunjie <811860136@qq.com>
2019-08-22 07:46:24 +0800

    添加工具类

---
+++ src/main/java/com/wf/common/vo/AbstractRequest.java
@@ -0,0 +1,15 @@
+package com.wf.common.vo;
+
+import java.io.Serializable;
+
+
+public abstract class AbstractRequest implements Serializable{
+
+    private static final long serialVersionUID = 1717442845820713651L;
+    public abstract void requestCheck();
+
+    @Override
+    public String toString() {
+        return "AbstractRequest{}";
+    }
+}
